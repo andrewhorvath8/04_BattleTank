0d6a86a2de1fbc3197de68cdb2ab541a5b427974
Andras Horvath <andrew.horvath8@gmail.com>
2019-05-20 00:59:22 +0200

    Logging controlled tank's name

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "BattleTank.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlled tank is: %s"), *(ControlledTank->GetName()));
+	}
+	
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
